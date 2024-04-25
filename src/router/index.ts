a4110192f35093680dc5890d239931f5d8388d19
Guilherme <guilherme.asalgado@gmail.com>
2024-04-24 23:15:23 -0300

    Initial commit

---
+++ src/router/index.ts
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+import SignInView from '@/views/SignInView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/signin',
+      name: 'signin',
+      component: SignInView
+    }
+  ]
+})
+
+export default router
