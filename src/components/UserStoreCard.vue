c78ba6ff045ed0f8bc239f781e4a79c274f3565d
Guilherme <guilherme.asalgado@gmail.com>
2024-05-22 00:02:04 -0300

    Added store table columns

--- src/components/UserStoreCard.vue
+++ src/components/UserStoreCard.vue
@@ -3,10 +3,12 @@
         <DataTable
             :value="stores" paginator 
             :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]"
-            sortField="id" :sortOrder="-1"
+            sortField="updated_at" :sortOrder="-1"
             tableStyle="min-width: 50rem">
                 <Column field="id" header="Id" sortable style="width: 25%"></Column>
                 <Column field="name" header="Name" sortable style="width: 25%"></Column>
+                <Column field="created_at" header="Created at" sortable style="width: 25%"></Column>
+                <Column field="updated_at" header="Updated at" sortable style="width: 25%"></Column>
         </DataTable>
     </div>
 </template>
